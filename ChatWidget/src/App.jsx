86ba9ba578b8023decfda6452463e67c7ab8b3fd
Raam <raam.sanghani@gmail.com>
2024-02-28 17:56:40 -0600

    Fleshed out the basic template for a pop in pop out modal and widget via the Ably blog guide

---
+++ ChatWidget/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import ChatWidget from './components/ChatWidget';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ChatWidget />
+    </>
+  )
+}
+
+export default App
---
+++ ChatWidget/src/components/ChatWidget/index.jsx
@@ -0,0 +1,56 @@
+import { styles } from "./styles";
+import { BsFillChatFill } from "react-icons/bs";
+import {useState, useEffect, useRef} from "react";
+import ModalWindow from "./ModalWindow";
+
+function ChatWidget() {
+    const [hovered, setHovered] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const widgetRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+          if (widgetRef.current && !widgetRef.current.contains(event.target)) {
+            setVisible(false);
+          }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+      }, [widgetRef]);
+    
+    return (
+        <div ref={widgetRef}>
+        {/* Chat Button Component */}
+        <ModalWindow visible={visible}/>
+        <div
+        onClick={() => setVisible(!visible)}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={()=> setHovered(false)}
+          style={{
+            ...styles.chatWidget,
+            ...{border: hovered ? "1px solid black" : ""},
+            ...{backgroundColor: hovered ? "#68c66b" : styles.chatWidget.backgroundColor}
+            
+          }}
+        >
+          {/* Inner Container */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <BsFillChatFill size={20} color="white" />
+            {/* Button Text */}
+            <span style={styles.chatWidgetText}>Wanna Chat?</span>
+          </div>
+        </div>
+      </div>      
+
+    );
+}
+
+export default ChatWidget;
